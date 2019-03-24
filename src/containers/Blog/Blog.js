10cb4cf4a87d738622116cda3211bc14f08bd738
Magnus Lundvall <lundvall@gmail.com>
2019-03-24 21:53:31 +0100

    lazy loading component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
 import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
-import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
-// import classes from './Blog.module.css';
+
 import './Blog.css';
+import Posts from './Posts/Posts';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost');
+});
+// import classes from './Blog.module.css';
+
 
 class Blog extends Component {
 
   state = {
-    auth: false
+    auth: true
   };
 
   render() {
@@ -29,8 +35,9 @@ class Blog extends Component {
         {/*<Route path="/" exact render={() => <h1>Home</h1>}/>*/}
         {/*<Route path="/" exact render={() => <h1>Home 2</h1>}/>*/}
         <Switch>
-          {this.state.auth ? <Route path={"/new-post"} component={NewPost}/> : null}
+          {this.state.auth ? <Route path={"/new-post"} component={AsyncNewPost}/> : null}
           <Route path={"/posts"} component={Posts}/>
+          {/*<Route render={() => <h1>Not Found!</h1>}/>*/}
           <Redirect from="/" to="/posts"/>
         </Switch>
       </div>
